c3fe925a2211243c9efa5b2bbab1fc8b6b804f61
Dosunmu Afeez <42891608+Haayzeed@users.noreply.github.com>
2019-05-17 09:23:31 +0100

    Add files via upload

---
+++ cv-builder/cv-generator.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width initial-scale=1.0">
+	<title>CV-Generator</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome.css">
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+	<div class="container">
+		<!-- Name of User -->
+		<div class="name">
+			<h1>John Smith</h1>
+			<h5>IT Project Manager</h5>
+		</div>
+		<div class="row mt-4">
+			<!-- Social Media account of user -->
+			<div class="col-md-12 social-media-top">
+				<div class="row">
+					<div class="col-md-4">
+						<p><span class="fa fa-phone p-1 m-1"></span>0812-345-6789</p>
+					</div>
+					<div class="col-md-4">
+						<p><span class="fa fa-linkedin p-1 m-1"></span>Linked.com/johnnutw</p>
+					</div>
+					<div class="col-md-4"></div>
+				</div>
+			</div>
+			<div class="col-md-12 social-media-bottom">
+				<div class="row">
+					<div class="col-md-4">
+						<p><span class="fa fa-envelope p-1 m-1"></span>j.smith@uptowork.com</p>
+					</div>
+					<div class="col-md-4">
+						<p><span class="fa fa-twitter p-1 m-1"></span>@johnsmithutw</p>
+					</div>
+					<div class="col-md-4"></div>
+				</div>
+			</div>
+			<!-- about user -->
+			<div class="col-md-12">
+				<p class="text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+			</div>
+		</div>
+		<!-- Experience -->
+		<div class="row">
+			<div class="col-md-12 experience">
+				<div class="card border-0" style="">
+					<div class="card-body pl-0">
+					    <h4 class="card-title mb-0"><span class="fa fa-briefcase fa-icon p-1 mr-2"></span>Experience</h4>
+					    <hr class="mt-0">
+					    <div class="row mb-4">
+						    <div class="col-md-2 font-weight-bold">
+						    	2006-12-present
+						    </div>
+						    <div class="col-md-10">
+						    	<h5>Senior Project Manager</h5>
+						    	<p class="mb-1"><i>Seton Hospital, ME</i></p>
+						    	<ul class="list-group ml-4">
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    	</ul>
+						    </div>
+					    </div>
+					    <div class="row">
+						    <div class="col-md-2 font-weight-bold">
+						    	2004-09-06-11
+						    </div>
+						    <div class="col-md-10">
+						    	<h5>Junior Project Manager</h5>
+						    	<p class="mb-1"><i>Seton Hospital, ME</i></p>
+						    	<ul class="list-group ml-4">
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    	</ul>
+						    </div>
+					    </div>
+					</div>
+				</div>	
+			</div>
+		</div>
+
+		<!-- education -->
+		<div class="row">
+			<div class="col-md-12 education">
+				<div class="card border-0">
+					<div class="card-body pl-0">
+					    <h4 class="card-title mb-0"><span class="fa fa-graduation-cap fa-icon p-1 mr-2"></span>Education</h4>
+					    <hr class="mt-0">
+					    <div class="row mb-4">
+						    <div class="col-md-2 font-weight-bold">
+						    	1996-09-01-05
+						    </div>
+						    <div class="col-md-10">
+						    	<h5>BS/MS in Computer Science, University of Maryland</h5>
+						    	<ul class="list-group ml-4">
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    		<li class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+						    	</ul>
+						    </div>
+					    </div>
+					</div>
+				</div>	
+			</div>
+		</div>
+
+		<!-- skills -->
+		<div class="row">
+			<div class="col-md-12 skills">
+				<div class="card border-0" style="">
+					<div class="card-body pl-0">
+					    <h4 class="card-title mb-0"><span class="fa fa-wrench fa-icon p-1 mr-2"></span>Skills</h4>
+					    <hr class="mt-0">
+					    <div class="row mb-4">
+						    <div class="col-md-2 font-weight-bold">
+						    </div>
+						    <div class="col-md-10">
+						    	<ul class="list-group">
+						    		<li class=""><b>Business Process Improvement</b> - history of successful innovations leading to cost savings.</li>
+						    		<li class=""><b>Vendor Management</b> - proven track record of managing vendors in project with budgets of over &#8358;1,000,000.</li>
+						    		<li class=""><b>Project Scheduling</b> - Over 90% of projects led were finished in due time.</li>
+						    		<li class=""><b>Sales Analysis</b> - Backgrounds in IT sales with deep understanding of negotiating contracts.</li>
+						    	</ul>
+						    </div>
+					    </div>
+					</div>
+				</div>	
+			</div>
+		</div>
+
+		<!-- software -->
+		<div class="row">
+			<div class="col-md-12 skills">
+				<div class="card border-0" style="">
+					<div class="card-body pl-0">
+					    <h4 class="card-title mb-0"><span class="fa fa-desktop fa-icon p-1 mr-2"></span>Software</h4>
+					    <hr class="mt-0">
+					    <div class="row mb-2">
+						    <div class="col-md-2 font-weight-bold mb-0">
+						    </div>
+						    <div class="col-md-10 mb-0">
+						    	<div class="row">
+						    		<div class="col-md-10">
+						    			Micro Project, MS Windows Server, LINUX/UNIX
+						    		</div>
+						    		<div class="col-md-2 pr-0 pb-0">
+						    			<p class="text-right mb-0">
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    			</p>
+						    			<p class="text-right mt-0 pt-0 pb-0 mb-0">Excellent</p>
+						    		</div>
+						    	</div>
+						    </div>
+					    </div>
+					    <div class="row mb-2">
+						    <div class="col-md-2 font-weight-bold mt-0">
+						    </div>
+						    <div class="col-md-10 mt-0">
+						    	<div class="row">
+						    		<div class="col-md-10">
+						    			MS Windows Server
+						    		</div>
+						    		<div class="col-md-2 pr-0 pb-0">
+						    			<p class="text-right mb-0">
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square" style="color: lightgray"></span>
+						    			</p>
+						    			<p class="text-right mt-0 pt-0 pb-0 mb-0">Very Good</p>
+						    		</div>
+						    	</div>
+						    </div>
+					    </div>
+					     <div class="row mb-2">
+						    <div class="col-md-2 font-weight-bold mt-0">
+						    </div>
+						    <div class="col-md-10 mt-0">
+						    	<div class="row">
+						    		<div class="col-md-10">
+						    			MS Windows Server
+						    		</div>
+						    		<div class="col-md-2 pr-0 pb-0">
+						    			<p class="text-right mb-0">
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square"></span>
+						    				<span class="fa fa-square" style="color: lightgray"></span>
+						    			</p>
+						    			<p class="text-right mt-0 pt-0 pb-0 mb-0">Very Good</p>
+						    		</div>
+						    	</div>
+						    </div>
+					    </div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>	
+</div>
+</body>
+</html>
